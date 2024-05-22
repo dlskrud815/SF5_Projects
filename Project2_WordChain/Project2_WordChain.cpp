b48c578321a048f43a66af409c82a6c3aefcc62c
dlskrud815 <dlskrud815@gmail.com>
2024-05-22 16:31:01 +0900

    Project2_WordChain 코드 완성

--- Project2_WordChain/Project2_WordChain.cpp
+++ Project2_WordChain/Project2_WordChain.cpp
@@ -1,20 +1,97 @@
-﻿// Project2_WordChain.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿// 작성자: 이나경
+// 2024.05.22.
+// 
+// - 프로젝트 2. 타임어택 끝말잇기 게임 -
+// 1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
+// 2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
+// 3) 단, 한번 입력된 단어는 입력하지 못하게 하기
 
 #include <iostream>
+#include <ctime>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int errType = 0;
+	string word;
+	char last = ' ';
+	vector <string> wordChain = { "airplane" };
+
+	cout << "* 타임어택 끝말잇기 게임" << endl << endl
+		<< "- 제한시간은 30초 입니다. -" << endl;
+
+	clock_t startTime = clock();
+	while (1)
+	{
+		cout << endl;
+		for (int i = 0; i < wordChain.size(); i++)
+		{
+			cout << wordChain[i];
+
+			if (i != wordChain.size() - 1)
+			{
+				cout << " -> ";
+			}
+			else
+			{
+				//마지막 알파벳
+				last = wordChain[i][wordChain[i].length() - 1];
+			}
+		}
+
+		cout << endl << "다음 단어를 입력하세요 : ";
+		cin >> word;
+
+		while (1)
+		{
+			if (word[0] != last)
+			{
+				errType = 1;
+			}
+			else
+			{
+				for (int i = 0; i < wordChain.size(); i++)
+				{
+					if (word == wordChain[i])
+					{
+						errType = 2;
+						break;
+					}
+				}
+			}
+
+			switch (errType)
+			{
+			case 1:
+				cout << endl << "* 마지막 알파벳 " << last << "(으)로 시작하는 단어를 입력해주세요." << endl << endl;
+				break;
+			case 2:
+				cout << endl << "* 중복된 단어입니다." << endl << endl;
+				break;
+			}
+
+			if (!errType) break;
+			else
+			{
+				errType = 0;
+				cout << "[재입력]" << endl << "다음 단어를 입력하세요 : ";
+				cin >> word;
+			}
+		}
+
+		clock_t endTime = clock();
+
+		if ((endTime - startTime) / CLOCKS_PER_SEC > 30)
+		{
+			cout << "타임 오버!" << endl;
+			break;
+		}
+
+		wordChain.push_back(word);
+	}
+
+	cout << endl << "게임 종료!" << endl
+		<< "총 입력한 단어 개수 : " << wordChain.size() << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
