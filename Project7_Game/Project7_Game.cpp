413094368a9576b2409ede306a4afb36bb2cf34a
dlskrud815 <dlskrud815@gmail.com>
2024-05-24 20:23:01 +0900

    난수 생성 함수 삭제

--- Project7_Game/Project7_Game.cpp
+++ Project7_Game/Project7_Game.cpp
@@ -27,16 +27,6 @@
 
 using namespace std;
 
-int GetRandomNum(int num)
-{
-	// 균등 분포 난수 생성
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<int> dis(1, num);
-
-	return dis(gen);
-}
-
 void Loading(int sec)
 {
 	int i = 0;
